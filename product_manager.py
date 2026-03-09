f2e7f51c58cb9bad50300cfb4da02b4b3f06d8c3
Nando <fernandosurcochoa@gmail.com>
2026-03-08 21:13:05 -0500

    Fix flake8 lint errors

--- product_manager.py
+++ product_manager.py
@@ -1,17 +1,22 @@
 import json
 import os
 
+
 FILE_PATH = "products.json"
+
 
 def load_data():
     if not os.path.exists(FILE_PATH):
         return []
+
     with open(FILE_PATH, "r") as f:
         return json.load(f)
+
 
 def save_data(data):
     with open(FILE_PATH, "w") as f:
         json.dump(data, f, indent=4)
+
 
 def create_product(product):
     data = load_data()
@@ -19,8 +24,10 @@ def create_product(product):
     save_data(data)
     return product
 
+
 def read_products():
     return load_data()
+
 
 def update_product(product_id, new_data):
     data = load_data()
@@ -32,6 +39,7 @@ def update_product(product_id, new_data):
             return product
 
     return None
+
 
 def delete_product(product_id):
     data = load_data()
