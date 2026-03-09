a05297db469a6f50a3bffeabceda5e58cc24b9ae
Nando <fernandosurcochoa@gmail.com>
2026-03-08 21:16:44 -0500

    Fix flake8 import order

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import product_manager as pm
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def test_create_product():
